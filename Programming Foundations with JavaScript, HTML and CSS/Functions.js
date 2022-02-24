00a0fcf70373c56c95732c4f44feb8bb82674834
EdgarHdz17 <condoritos_170696@hotmail.com>
2022-02-24 14:24:44 -0600

    Git Init

---
+++ Programming Foundations with JavaScript, HTML and CSS/Functions.js
@@ -0,0 +1,29 @@
+var image;
+
+function upload() {
+  var imgcanvas = document.getElementById("can");
+  var fileinput = document.getElementById("finput");
+  image= new SimpleImage(fileinput);
+  image.drawTo(imgcanvas);
+}
+
+function makeRed () {
+  for (var pixel of image.values()) {
+    var avg=(pixel.getBlue()-pixel.getGreen());
+    pixel.setBlue(avg);
+    pixel.setGreen(avg);
+  }
+  var imgcanvas = document.getElementById("can");
+  image.drawTo(imgcanvas);
+}
+
+function makeGray() {
+  for (var pixel of image.values()) {
+    var avg =(pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+    pixel.setRed(avg);
+    pixel.setGreen(avg);
+    pixel.setBlue(avg);
+  }
+  var imgcanvas = document.getElementById("can");
+  image.drawTo(imgcanvas);
+}
